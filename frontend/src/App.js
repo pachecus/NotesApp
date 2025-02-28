8533dcc89b36301a774ce4b6bb7052c7d1967ae0
pachecus <126217007+pachecus@users.noreply.github.com>
2025-02-28 16:49:23 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { NotePage } from './pages/NotePage';
+
+function App() {
+  return (
+    <div className="App">
+      <NotePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/NotePage.jsx
@@ -0,0 +1,8 @@
+export const NotePage = () => {
+    return (
+        <div>
+            <h1>Mis notas</h1>
+            
+        </div>
+    )
+}
