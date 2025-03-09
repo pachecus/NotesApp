a79b8ca19384fc05cc368992cc803cd32429b76c
pachecus <126217007+pachecus@users.noreply.github.com>
2025-03-09 00:24:16 -0300

    Crear notas agrega categorias (solo visual)
    Se pueden agregar categorias a las notas al crearlas pero solo visualmente desde la pagina web, actualmente no se guardan en la base de datos estas categorias

--- frontend/src/components/NoteForm.jsx
+++ frontend/src/components/NoteForm.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useContext } from 'react';
 import { NoteContext } from '../context/NoteContext';  // NoteContext to add new notes
+import { CategoriesContext } from '../context/CategoriesContext';
 
 
 export const NoteForm = () => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const [ noteCategories, setNoteCategories ] = useState([]);
     const { addNote } = useContext(NoteContext); // addNote adds a new note to the context and to the database
+    const { categories } = useContext(CategoriesContext);
   
     // When a new note is submmited its added to the context and the title and content of the form are emptied
     const handleSubmit = (e) => {
@@ -14,6 +17,20 @@ export const NoteForm = () => {
       addNote({ title, content });
       setTitle('');
       setContent('');
+    };
+
+
+
+    const handleClick = (e) => {
+      console.log('handle Click');
+      if (e.target.value === "") {return;}
+      const category = JSON.parse(e.target.value);
+
+      // Si la categoria no fue seleccionada
+      if (!noteCategories.some((cat) => cat.id === category.id)) {
+        setNoteCategories([...noteCategories, category]);
+      }
+
     };
   
     return (
@@ -34,6 +51,27 @@ export const NoteForm = () => {
             value={content}
             onChange={(e) => setContent(e.target.value)}
           ></textarea>
+          {categories.length === 0 ? ( 
+                <p>There are no categories yet.</p> // Id there are no notes
+            ) : (
+                <select onChange={handleClick}>
+                <option value="">Select the categories</option>
+                {categories.map((category) => ( // For each note show ,title, content, edit button that redirects to edit page, delete button and archived/unarchived button
+                    <option key={category.id} value={JSON.stringify(category)}>{category.name}</option>
+                ))}
+                </select>
+            )}
+            {noteCategories.length === 0 ? ( 
+                <p>You have not selected categories for this note yet</p> // Id there are no notes
+            ) : (
+                <ul>
+                {noteCategories.map((category) => ( // For each note show ,title, content, edit button that redirects to edit page, delete button and archived/unarchived button
+                    <li key={category.id}>
+                      <p>{category.name}</p>
+                    </li>
+                ))}
+                </ul>
+            )}
         </div>
         <button type="submit">Save Note</button>
       </form>
